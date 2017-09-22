83afe051dadfbc62a7b5ccc5ba5e2d65fb953107
Junnel Reboquio <jreboqui@ucsd.edu>
2017-09-22 07:00:43 -0700

    Added image and listview

--- app/src/main/java/com/example/junnel_lalaine/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/junnel_lalaine/starbuzz/TopLevelActivity.java
@@ -1,7 +1,13 @@
 package com.example.junnel_lalaine.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+
+                if (position == 0){
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
