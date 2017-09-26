08bcef4f34bc2cd4f4728a9a4c92a40637c77ea3
Junnel Reboquio <jreboqui@ucsd.edu>
2017-09-25 19:45:25 -0700

    Done with drinks

--- app/src/main/java/com/example/junnel_lalaine/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/example/junnel_lalaine/starbuzz/DrinkActivity.java
@@ -2,12 +2,35 @@ package com.example.junnel_lalaine.starbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DrinkActivity extends AppCompatActivity {
+
+    public static final String EXTRA_DRINKNO = "drinkNo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+
+        Drink drink = Drink.drinks[drinkNo];
+
+        //Populate the activity set in the xml file
+
+        ImageView photo = (ImageView) findViewById(R.id.photo);
+
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(drink.getDescription());
     }
 }
